017d92ea3a1ba345e6641b09b5df61a523f7c22d
Mike Solomon <mike@wavr.so>
2021-12-14 17:39:16 -0800

    Adds utility functions

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,56 @@
-import { tsCycleEnvToPSCycleEnv, psCycleEnvToTSCycleEnv, tsCycleToPSCycle, psCycleToTSCycle } from './ps';
+import {
+  tsCycleEnvToPSCycleEnv,
+  psCycleEnvToTSCycleEnv,
+  tsCycleToPSCycle,
+  psCycleToTSCycle,
+} from './ps';
+import { Cycle, Maybe, NonEmptyArray, Note, TimeIs } from './types';
+
+const s2c = (s: string): Cycle<Maybe<Note<void>>> => ({
+  type: 'singleNote',
+  value: {
+    val: {
+      sampleFoT: s,
+      forward: true,
+      rateFoT: (_: TimeIs<void>) => 1.0,
+      bufferOffsetFoT: (_: TimeIs<void>) => 0.0,
+      volumeFoT: (_: TimeIs<void>) => 1.0,
+    },
+    env: { weight: 1.0 },
+  },
+});
+
+const i = <V>(nel: NonEmptyArray<Cycle<V>>): Cycle<V> => ({
+  type: 'internal',
+  value: {
+    nel,
+    env: { weight: 1.0 },
+  },
+});
+
+const b = <V>(nel: NonEmptyArray<Cycle<V>>): Cycle<V> => ({
+  type: 'branching',
+  value: {
+    nel,
+    env: { weight: 1.0 },
+  },
+});
+
+const x = <V>(nel: NonEmptyArray<Cycle<V>>): Cycle<V> => ({
+  type: 'simultaneous',
+  value: {
+    nel,
+    env: { weight: 1.0 },
+  },
+});
 
 export {
   tsCycleEnvToPSCycleEnv,
   psCycleEnvToTSCycleEnv,
   tsCycleToPSCycle,
   psCycleToTSCycle,
+  s2c,
+  i,
+  b,
+  x,
 };
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,52 +1,17 @@
-import { tsCycleToPSCycle, psCycleToTSCycle } from '../src';
-import { Cycle } from '../src/types';
+import { tsCycleToPSCycle, psCycleToTSCycle, s2c, i, b, x } from '../src';
+import { Cycle, Maybe, Note } from '../src/types';
 
 describe('ts cycle to ps cycle', () => {
   it('does a simple two way conversion with 0 loss', () => {
-    const cycle: Cycle<string> = {
-      type: 'internal',
-      value: {
-        nel: [
-          { type: 'singleNote', value: { val: 'a', env: { weight: 1.0 } } },
-        ],
-        env: { weight: 1.0 },
-      },
-    };
+    const cycle: Cycle<Maybe<Note<void>>> = i([s2c('a')]);
     expect(psCycleToTSCycle(tsCycleToPSCycle(cycle))).toEqual(cycle);
   });
   it('does a more involved two way conversion with 0 loss', () => {
-    const cycle: Cycle<string> = {
-      type: 'internal',
-      value: {
-        nel: [
-          {
-            type: 'simultaneous',
-            value: {
-              nel: [
-                {
-                  type: 'singleNote',
-                  value: { val: 'a', env: { weight: 1.0 } },
-                },
-              ],
-              env: { weight: 1.0 },
-            },
-          },
-          {
-            type: 'internal',
-            value: {
-              nel: [
-                {
-                  type: 'singleNote',
-                  value: { val: 'a', env: { weight: 1.0 } },
-                },
-              ],
-              env: { weight: 1.0 },
-            },
-          },
-        ],
-        env: { weight: 1.0 },
-      },
-    };
+    const cycle: Cycle<Maybe<Note<void>>> = i([
+      s2c('a'),
+      b([s2c('a'), x([s2c('b')]), s2c('c')]),
+      s2c('c'),
+    ]);
     expect(psCycleToTSCycle(tsCycleToPSCycle(cycle))).toEqual(cycle);
   });
 });
